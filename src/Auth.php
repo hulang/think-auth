96bb41b693bf336b53509a566bb19846f241c89f
快乐浪子哥 <hulangfy@163.com>
2021-05-17 14:48:54 +0800

    update

--- src/Auth.php
+++ src/Auth.php
@@ -10,7 +10,11 @@ use think\Facade;
  * auth 权限检测入口
  * 
  * @see \think\auth\service\Auth
+ * @package think
  * @mixin \think\auth\service\Auth
+ * @method static mixed check(string|array $name, int $uid, int $type = 1, string $mode = 'url', string $relation = 'or') 检查权限
+ * @method static mixed rules(int $uid = 0, int $type = 1) 返回用户的所有规则表
+ * @method static mixed roles(int $uid = 0, string $field = '') 获取用户所有角色信息
  */
 class Auth extends Facade
 {
