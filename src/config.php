52e0e0da1cd49ab5d4eb8c6b32eaf25006a8b98a
hulangfy <hulangfy@163.com>
2021-05-17 11:26:12 +0800

    Revert "update"
    This reverts commit d40cc64b52882fc6fa9b69f8d6c68e1895fc86c7.

--- src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'auth_on'           => 1, // 权限开关
-    'auth_type'         => 1, // 认证方式，1为实时认证；2为登录认证。
-    'auth_user'         => 'member', // 用户信息不带前缀表名
-];
