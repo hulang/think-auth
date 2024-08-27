89e6be0c2809a447056673adcb04dad87396f817
快乐浪子哥 <4961831+hulang@users.noreply.github.com>
2024-08-27 15:46:25 +0800

    Update

--- src/config.php
+++ src/config.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'auth_on'           => 1, // 权限开关
-    'auth_type'         => 1, // 认证方式，1为实时认证；2为登录认证。
-    'auth_user'         => 'admin', // 用户信息不带前缀表名
+    'auth_on' => 1, // 权限开关
+    'auth_type' => 1, // 认证方式，1为实时认证；2为登录认证。
+    'auth_user' => 'admin', // 用户信息不带前缀表名
 ];
